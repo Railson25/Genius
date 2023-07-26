b260719cdf523fbca0d99a2204425272bea2f3ff
Railson25 <railsonsantiago123@gmail.com>
2023-07-26 11:36:12 -0300

    <feat>: Sidebar component

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,7 @@
 export default function dashboardPage() {
   return (
-    <div className=" flex items-center justify-center h-full">
-      <h1 className='text-6xl'>Dashboard page (Protected) </h1>
+    <div>
+      <h1 className='text-6xl'>Dashboard page (Protected)</h1>
     </div>
   )
 }
