0b68f956fd26b8d876923a2035c18bb1c031c91e
Railson25 <railsonsantiago123@gmail.com>
2023-07-26 11:48:35 -0300

    <feat>: Highligth components on sidebar by dafault

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,8 +1,10 @@
 "use client"
 
+import {Montserrat} from 'next/font/google'
+import {usePathname} from 'next/navigation'
+
 import Image from 'next/image'
 import Link from "next/link";
-import {Montserrat} from 'next/font/google'
 import { cn } from '@/lib/utils';
 
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from 'lucide-react';
@@ -54,6 +56,7 @@ const routes = [
 ]
 
 const Sidebar = () => {
+    const pathName = usePathname()
     return(
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
             <div className="px-3 py-2 flex-1">
@@ -74,7 +77,11 @@ const Sidebar = () => {
                         <Link
                             href={route.href}
                             key={route.href}
-                            className='text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition'
+                            className={cn('text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition',
+                            pathName === route.href 
+                            ? "text-white bg-white/10"
+                            : "text-zinc-400"
+                            )}
                         >
                             <div className='flex items-center flex-1'>
                                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
