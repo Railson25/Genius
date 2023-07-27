10499c52f4a57cee7beee7bc730a887fd815b607
Railson25 <railsonsantiago123@gmail.com>
2023-07-27 16:54:36 -0300

    <feat>: API Limit

---
+++ lib/api-limit.ts
@@ -0,0 +1,49 @@
+import {auth} from '@clerk/nextjs'
+
+import prismadb from './prismadb'
+import { MAX_FREE_COUNTS } from '@/constants'
+
+export const increaseApiLimit = async () => {
+    const {userId} = auth()
+
+    if(!userId){
+        return
+    }
+
+    const userApiLimit = prismadb.userApiLimit.findUnique({
+        where: {
+            userID: userId
+        }
+    })
+
+    if(userApiLimit) {
+        await prismadb.userApiLimit.update({
+            where: {userID: userId},
+            data: {count: userApiLimit.count + 1}
+        })
+    }else {
+        await prismadb.userApiLimit.create({
+            data: {userID: userId, count: 1}
+        })
+    }
+}
+
+export const checkApiLimit = async () => {
+    const {userId} = auth()
+
+    if(!userId){
+        return false
+    }
+
+    const userApiLimit = await prismadb.userApiLimit.findUnique({
+        where: {
+            userID: userId
+        }
+    })
+
+    if(!userApiLimit || userApiLimit.count < MAX_FREE_COUNTS){
+        return true
+    }else{
+        return false
+    }
+}
