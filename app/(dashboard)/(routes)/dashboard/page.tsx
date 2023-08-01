32146b8ee958603a44fa506a263d6a521f0f50d6
Railson25 <railsonsantiago123@gmail.com>
2023-08-01 16:30:50 -0300

    <refactor>: Nmae of function in Dashboard page

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -8,7 +8,7 @@ import { useRouter } from "next/navigation"
 import {tools} from '@/constants'
  
 
-export default function dashboardPage() {
+export default function DashboardPage() {
   const router = useRouter()
   return (
     <div>
